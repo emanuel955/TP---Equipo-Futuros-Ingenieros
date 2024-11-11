3c2826780b92dc99274ae821b700795dce2d7b36
Nicolas Granata <bngranata1@gmail.com>
2024-11-10 21:09:10 -0300

    Integracion de Img de about-us.html

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def rooms():
     return render_template('rooms.html')
 
+@app.route('/about-us.html')
+def aboutus():
+    return render_template('about-us.html')
+
 if __name__ == '__main__':
     app.run(debug=True)

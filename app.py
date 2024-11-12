8cba9e965f2d86302998388ac47d0499bbc7c95e
Nicolas Granata <bngranata1@gmail.com>
2024-11-11 21:22:07 -0300

    Corrección de API y template Hoteles

--- app.py
+++ app.py
@@ -34,9 +34,5 @@ def registrarse():
 def reserva():
     return render_template('reserva.html')
 
-@app.route('/hoteles')
-def hoteles():
-    return render_template('hoteles.html')
-
 if __name__ == '__main__':
     app.run(debug=True)

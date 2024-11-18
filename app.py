586d8c8799e5e7e79ef47f822e6672e9b1075ad4
Roku <riquelme01.lucas@gmail.com>
2024-11-18 15:28:11 -0300

    ID#31,32,26 Login, registro y tabla de reserva

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from mysql.connector import connect, Error
 import requests
 API_URL = 'http://flask_api:8080/api/v1/'
 
+
 app = Flask(__name__)
+app.secret_key = "clave_ultra_secreta"
 
 # Configuración para conectar a MySQL
 app.config['MYSQL_HOST'] = 'mysql_db'  # Nombre del servicio MySQL en Docker Compose
@@ -35,14 +37,19 @@ def test_db():
 
 @app.route('/')
 def index():
+    #if 'usuario' not in session:  
+    #    flash('Por favor, inicia sesión para continuar.')
+    #    return redirect(url_for('login'))
+    #Usar para cuando se mande el POST request para ver la disponibilidad de los hoteles
+    #Si no esta el usuario -> al login, de lo contrario se hace la request de POST
     try:
         response = requests.get(API_URL+'hoteles')
         response.raise_for_status()
         hoteles = response.json()
 
-        # response = requests.get(API_URL+'testimonios')
-        # response.raise_for_status()
-        # testimonios = response.json()
+        #response = requests.get(API_URL+'testimonios')
+        #response.raise_for_status()
+        #testimonios = response.json()
         testimonios = []
 
     except requests.exceptions.RequestException as e:
@@ -63,21 +70,57 @@ def hoteles():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
 @app.route('/reserva')
 def reserva():
     return render_template('reserva.html')
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        mail = request.form['mail']
+        password = request.form['password']
+        datos = {'mail':mail,'password':password}
+
+        try:
+            usuario = requests.post(API_URL+'login',json=datos)
+            if usuario.status_code == 201:
+                session['usuario'] = mail
+                return redirect(url_for('index'))
+            else:
+                flash('Correo o contraseña incorrectos.')
+        except Exception as e:
+            flash(f'Error de conexión al servidor: {e}')
+
     return render_template('login.html')
 
-@app.route('/registrarse')
+@app.route('/registrarse',methods=['GET','POST'])
 def registrarse():
-    return render_template('registrarse.html')
+    if request.method == 'POST':
+        mail = request.form['mail']
+        password = request.form['password']
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        datos = {'mail': mail,
+                 'password': password,
+                 'nombre': nombre,
+                 'apellido': apellido
+                } 
+        try:
+            response = requests.post(API_URL+'registrarse',json=datos)
+            if response.status_code == 201:
+                return redirect(url_for('login'))
+            if response.status_code == 400:
+                flash("El email que ingreso ya esta registrado. Ingrese uno diferente.")
+                return render_template('registro.html',mail = mail,nombre = nombre,apellido = apellido)
+
+        except requests.exceptions.RequestException as e:
+            print (f"Error sending data: {e}")
+    return render_template('registro.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario', None) 
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug=True)
